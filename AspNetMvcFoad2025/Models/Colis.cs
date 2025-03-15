cc30b0c5fb68736ce1c074aca71677a783c5d7b3
Mouhamad Gueye <seydinag023@gmail.com>
2025-03-15 15:06:50 +0000

    db connect

---
+++ AspNetMvcFoad2025/Models/Colis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvcFoad2025.Models
+{
+	public class Colis
+	{
+		[Key]
+		public int IdColis { get; set; }
+
+		[MaxLength(30, ErrorMessage ="Chaine trop longue"), Required(ErrorMessage = "Champs requis"), Display(Name="Code Colis")]
+        public string CodeColis { get; set; }
+
+        [MaxLength(30, ErrorMessage = "Chaine trop longue"), Required(ErrorMessage = "Champs requis"), Display(Name = "Libelle Colis")]
+        public string LibelleColis { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "Chaine trop longue"), Required(ErrorMessage = "Champs requis"), Display(Name = "Description Colis")]
+        public string DescriptionColis { get; set; }
+
+
+
+
+    }
+}
